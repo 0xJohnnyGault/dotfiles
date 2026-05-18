cf0dab0c733b4b9ecd21a8f78f16b77d3b25f766
Johnny Gault <john@multisiglabs.org>
2026-05-18 13:52:31 -0700

    wip

--- lib/packages-debian.sh
+++ lib/packages-debian.sh
@@ -32,16 +32,30 @@ install_packages() {
   curl --proto '=https' --tlsv1.2 -LsSf https://mise.run | sh
   curl --proto '=https' --tlsv1.2 -LsSf https://starship.rs/install.sh | sh -s -- --yes -b ~/.local/bin
   curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh -s -- --non-interactive
-  curl --proto '=https' --tlsv1.2 -LsSf https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
-  git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    curl --proto '=https' --tlsv1.2 -LsSf https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+    git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  fi
 }
 
 stow_packages() {
   info "Stowing packages..."
-  # atuin creates a .zshrc so we need to delete it
-  rm -f "$HOME/.zshrc"
+  backup_dotfiles
   stow -t "$HOME" -R zsh
   stow -t "$HOME" -R atuin
   stow -t "$HOME" -R starship
+}
+
+backup_dotfiles() {
+  local DATE
+  DATE=$(date +%Y%m%d%H%M%S)
+
+  for F in "$HOME/.zshrc" "$HOME/.zprofile" "$HOME/.config/atuin/config.toml" "$HOME/.config/starship.toml"; do
+    if [ -f "$F" ]; then
+      mv "$F" "${F}-${DATE}"
+      info "Backed up $F to ${F}-${DATE}"
+    fi
+  done
 }
